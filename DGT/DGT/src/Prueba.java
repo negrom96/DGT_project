669eac0a4e1ff6f2b56942ebeb3e6496685f6bee
negrom96 <Miguel.Negro@alu.uclm.es>
2016-11-08 13:12:59 +0100

    version 2 git

--- DGT/DGT/src/Prueba.java
+++ DGT/DGT/src/Prueba.java
@@ -2,6 +2,6 @@ public class Testing{
 	
 	public static void main (String[]args){
 	 
-		System.out.println("joseja, enhorabuena campeon");
+		System.out.println("joseja, enhorabuena campeon, ole ole ole");
 	}
 }
