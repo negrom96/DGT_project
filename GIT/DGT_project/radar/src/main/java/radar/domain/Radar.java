ec8d5c4c17a9e576a8fe7113b927ff750801b028
Pablo Palomino Gomez <Pablo.Palomino1@alu.uclm.es>
2016-12-01 11:01:16 +0100

    Iteracion 3: Apertura Expediente

--- GIT/DGT_project/radar/src/main/java/radar/domain/Radar.java
+++ GIT/DGT_project/radar/src/main/java/radar/domain/Radar.java
@@ -3,8 +3,9 @@ package radar.domain;
 import java.util.List;
 
 import org.hibernate.HibernateException;
-
 import edu.uclm.esi.iso2.multas.dao.GeneralDao;
+import edu.uclm.esi.iso2.multas.domain.Manager;
+import edu.uclm.esi.iso2.multas.domain.SanctionHolder;
 import edu.uclm.esi.iso2.multas.domain.Vehicle;
 
 public class Radar {
@@ -12,6 +13,7 @@ public class Radar {
 	private double velocidad_max;
 	
 	private List<Vehicle> coches;
+	private List<SanctionHolder> infracciones;
 	
 	public Radar (double velocidad_max) {
 		this.velocidad_max = velocidad_max;
@@ -28,14 +30,20 @@ public class Radar {
 		return state;
 	}
 	public void iniciar_radar () throws HibernateException {
+		String direccion = null;
+		int numeroExpediente;
+		Vehicle aux;
 		state = true;
-		coches = new GeneralDao().findAll(Vehicle.class);
+		coches = new GeneralDao<Vehicle>().findAll(Vehicle.class);
+		infracciones = new GeneralDao<SanctionHolder>().findAll(SanctionHolder.class);
 		do {
 			double velocidad = speed_random();
-			Vehicle aux;
 			if (velocidad > velocidad_max) {
-				aux = get_vehicles().get((int) Math.random() * (coches.size() - 1) + 0);
-				System.out.println("Coche: " + aux.getLicense());
+				aux = coches.get((int) Math.random() * (coches.size() - 1) + 0);
+				direccion = infracciones.get((int) Math.random() * (infracciones.size() - 1) + 0).getFullAddress();
+				System.out.println("Coche: " + aux.getLicense()+ " Localizacion: " + direccion);
+				// Apertura de Expediente
+				numeroExpediente = Manager.get().openInquiry(aux.getLicense(), velocidad, direccion, velocidad_max);
 			}
 		} while (state);
 	}
@@ -43,9 +51,5 @@ public class Radar {
 		double speed = 0.0;
 		speed = (double) (Math.random() * 199 + 0);
 		return speed;
-	}
-	
-	public List<Vehicle> get_vehicles () {
-		return coches;
 	}
 }
